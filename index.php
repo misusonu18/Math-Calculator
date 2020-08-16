3cb72824c3256dfc4ca37c0a3711c24088b1b977
misusonu18 <misusonu18@gmail.com>
2020-08-16 21:42:08 +0530

    refactor code

--- index.php
+++ index.php
@@ -2,32 +2,36 @@
 include 'layouts/header.php';
 ?>
 <div class="container">
-	<label class="h3"> 2D geometry calculators </label>
-	<hr class="bg-success"/>
-	<div class="row">
-		<div class="col-3">
-			<a href="area.php" class="btn btn-block">
-				<div class="card-body rounded bg-info justify-content-center">
-					Area Calculator
-				</div>
-			</a>
-		</div>
-
-		<div class="col-3">
-			<a href="perimeter.php" class="btn btn-block">
-				<div class="card-body rounded bg-info justify-content-center">
-					Perimeter Calculator
-				</div>
-			</a>
-		</div>
-
-		<div class="col-3">
-
-		</div>
-		<div class="col-3">
-
-		</div>
+	<div class="row justify-content-center mt-5">
+        <p class="h1"> 2D GEOMETRY CALCULATORS </p>
 	</div>
+    <hr class="bg-success"/>
+    
+    <div class="row">
+        <div class="col-4">
+            <a href="area.php" role="button" class="btn">
+                <div class="card-body custom-card rounded bg-info d-flex justify-content-center align-items-center">
+                    <p class="h5">AREA CALCULATOR</p>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-4">
+            <a href="perimeter.php" role="button" class="btn">
+                <div class="card-body custom-card rounded bg-info d-flex justify-content-center align-items-center">
+                    <p class="h5">PERIMETER CALCULATOR</p>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-3">
+
+        </div>
+        
+        <div class="col-3">
+
+        </div>
+    </div>
 </div>
 <?php
 include 'layouts/footer.php';
